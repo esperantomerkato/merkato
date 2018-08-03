4c3b5efc35501926656e2572e5bfe5c8a7c56b21
15chrjef <15chrjef@gmail.com>
2018-08-02 23:02:45 -0700

    (fix) remove logging of merkato config

--- merkato/utils/__init__.py
+++ merkato/utils/__init__.py
@@ -120,7 +120,6 @@ def generate_complete_merkato_configs(merkato_objects):
         complete_config['quote_volume'] = merkato['quote_volume']
         complete_config['base_volume'] = merkato['base_volume']
         merkato_complete_configs.append(complete_config)
-        print('complete_config', complete_config)
     return merkato_complete_configs
 
 
