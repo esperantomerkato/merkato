f4a4f1e9103c930176957d87a97bc36728dcb320
15chrjef <15chrjef@gmail.com>
2018-08-18 16:29:52 -0700

    fix volume update in rebalance orders

--- merkato/merkato.py
+++ merkato/merkato.py
@@ -145,9 +145,9 @@ class Merkato(object):
                     market_orders.append((amount, buy_price, BUY, tx_id,))
 
                 self.apply_filled_difference(tx, total_amount)
-                self.base_volume += total_amount * Decimal(float(price))
 
                 if float(price) < float(self.starting_price):
+                    self.base_volume += total_amount * Decimal(float(price))
                     update_merkato(self.mutex_UUID, BASE_VOLUME, float(self.base_volume))
 
             if tx[TYPE] == BUY:
@@ -162,8 +162,9 @@ class Merkato(object):
                     market_orders.append((amount, sell_price, SELL, tx_id))
 
                 self.apply_filled_difference(tx, total_amount)
-                self.quote_volume += total_amount
+                
                 if float(price) > float(self.starting_price):
+                    self.quote_volume += total_amount
                     update_merkato(self.mutex_UUID, QUOTE_VOLUME, float(self.quote_volume))
 
             insert_transaction(self.mutex_UUID, self.exchange.base, self.exchange.coin, float(self.spread), tx_id, orderid, float(price), float(filled_amount), tx['time'])
