7366701773ff4e69266b762c65d500b08b64f080
15chrjef <15chrjef@gmail.com>
2019-02-10 08:43:18 -0800

    (fix) missing scanner to find both gaps and double ups, and add graph display

--- missing_scanner.py
+++ missing_scanner.py
@@ -1,42 +1,57 @@
 from binance.client import Client
 from binance.enums import *
+from matplotlib.ticker import FuncFormatter
+from decimal import Decimal
+import matplotlib.pyplot as plt
+import numpy as np
 
 client = Client()
 
 orders = client.get_open_orders(symbol='XMRBTC', recvWindow=10000000)
-sorted_orders = sorted(orders, key=lambda order: order['price'])
+s_o = sorted(orders, key=lambda order: order['price'])
 
-# print('ord	ers', orders)
 
 def sort_orders(order):
     return float(order['price'])
 
-def find_problems(orders, step):
-    for i, order in enumerate(orders):
-        not_last_order = i != len(orders) -1
-        if not_last_order:
-            small = float(orders[i]['price'])
-            big = float(orders[i + 1]['price'])
-            small_big_div = big/small
-            if small_big_div > 1 + (step * 1.50):
-                print('ERROR GAP')
-                print('small', small)
-                print('big', big)
-
+def find_problems(orders, step, spread):
     orders.sort(key=sort_orders, reverse=True)
     for i, order in enumerate(orders):
         not_last_order = i != len(orders) -1
         if not_last_order:
             big = float(orders[i]['price'])
             small = float(orders[i + 1]['price'])
-            gap = (big-small)/small
+            gap = abs((big-small)/small)
             if gap < (step / 2):
                 print('ERROR DOUBLED UP')
                 print('small', small)
                 print('big', big)
+            if 1+gap > (1+step) * (1+spread):
+                print('ERROR GAP')
+                print('small', small)
+                print('big', big)
 
-find_problems(orders, .025)
+find_problems(orders, .0175, .025)
 
-trades = client.get_my_trades(symbol='XMRBTC', recvWindow=10000000)
+def get_filtered_amounts(orders):
+    return list(map(lambda x: x['origQty'], orders))
 
-print('treades', trades)
+def get_filtered_prices(orders):
+    return list(map(lambda x: round(float(x['price']) * 100000), orders))
+
+my_input = input('which section of orders? 1, 2, or 3? ')
+if my_input == '1':
+    list_to_present = s_o[:round(len(s_o)/3)]
+elif my_input == '2':
+    list_to_present = s_o[round(len(s_o)/3):round(len(s_o)/3*2)]
+elif my_input == '3':
+    list_to_present = s_o[round(len(s_o)/3*2):]
+
+x = np.arange(len(list_to_present))
+filterted_prices = get_filtered_prices(list_to_present)
+filtered_amounts = get_filtered_amounts(list_to_present)
+amounts = filtered_amounts
+fig, ax = plt.subplots()
+plt.bar(x, amounts)
+plt.xticks(x, filterted_prices)
+plt.show()
